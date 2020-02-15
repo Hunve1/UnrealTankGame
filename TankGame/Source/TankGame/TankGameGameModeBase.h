d67ae5c064f1267561bd800e7f1a37a4e7ed2eb3
Hunve1 <edelenyiszaboviktor@gmail.com>
2020-02-15 19:07:14 +0100

    Uploading the base of the prject

---
+++ TankGame/Source/TankGame/TankGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAME_API ATankGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
